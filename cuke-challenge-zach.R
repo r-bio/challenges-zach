db4b5beb198e24e35ef0954c2640edc398634f19
Zachary Emberts <emberts@ufl.edu>
2015-02-20 14:58:41 -0500

    Upload project

---
+++ cuke-challenge-zach.R
@@ -0,0 +1,17 @@
+download.file("http://r-bio.github.io/data/holothuriidae-specimens.csv", 
+              "data/holothuriidae-specimens.csv")
+
+download.file("http://r-bio.github.io/data/holothuriidae-nomina-valid.csv",
+              "data/holothuriidae-nomina-valid.csv")
+
+hol <- read.csv(file="data/holothuriidae-specimens.csv", stringsAsFactors=FALSE)
+nom <- read.csv(file="data/holothuriidae-nomina-valid.csv", stringsAsFactors=FALSE)
+head(hol)
+summary(hol)
+### there are 2,984 specimens
+summary(hol$dwc.institutionCode)
+hol$dwc.institutionCode
+?barplot()
+barplot(2982, hol$dwc.institutionCode)
+summary(hol$dwc.year)
+### oldest known specimen was from 2004
